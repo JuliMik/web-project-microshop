7372fef165007ab5e041e5158e35f40fc9b35673
JuliMik <gusachenkojulia@gmail.com>
2026-03-03 12:57:00 +0000

    create crud and dependencies

---
+++ core/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    "Base",
+    "DatabaseHalper",
+    "db_helper",
+    "Product",
+)
+
+from .base import Base
+from .db_helper import DatabaseHalper, db_helper
+from .product import Product
