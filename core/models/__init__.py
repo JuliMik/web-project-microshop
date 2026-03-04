c16f0639b18f15bf9981afb608470a706f9bafab
JuliMik <gusachenkojulia@gmail.com>
2026-03-04 10:25:06 +0000

    create user model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,8 +3,10 @@ __all__ = (
     "DatabaseHalper",
     "db_helper",
     "Product",
+    "User",
 )
 
 from .base import Base
 from .db_helper import DatabaseHalper, db_helper
 from .product import Product
+from .user import User
